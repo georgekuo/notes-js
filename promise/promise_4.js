b2f7248f8a559eecf8e3f3dc2bc06faaa2d5fb9a
KuoGeorge <ridnour@gmail.com>
2021-10-14 23:57:56 +0800

    [update] Remove semicolon

--- promise/promise_4.js
+++ promise/promise_4.js
@@ -22,14 +22,14 @@ theRoom
 /*--------------------------------------*/
 // Promise 順序:
 // const promise = new Promise(function (resolve){
-//   console.log("inner promise"); // 1. promise 內部先執行
-//   resolve(10);
-// });
+//   console.log("inner promise") // 1. promise 內部先執行
+//   resolve(10)
+// })
 // promise.then((value) => {
-//   console.log(value); // 3. 接著執行 then 裡面的程式碼
+//   console.log(value) // 3. 接著執行 then 裡面的程式碼
 //   console.log(300)    // 4
 // })
-// console.log("outer promise"); // 2. 執行 promise 之後的程式碼
+// console.log("outer promise") // 2. 執行 promise 之後的程式碼
 // inner promise
 // outer promise
 // 50
@@ -37,38 +37,38 @@ theRoom
 
 ///////////////////////////////
 // var promise = new Promise(function (resolve){
-//   console.log("1");  //1
-//   resolve(6);
-//   console.log("2"); // 2
-// });
+//   console.log("1")  //1
+//   resolve(6)
+//   console.log("2") // 2
+// })
 // promise.then(function(value){
-//   console.log(5);    // 5
+//   console.log(5)    // 5
 //   console.log(value) // 6
-// });
-// console.log("3");  // 3
-// console.log("4");  // 4
+// })
+// console.log("3")  // 3
+// console.log("4")  // 4
 
 ///////////////////////////////
-// console.log(1);  //1
+// console.log(1)  //1
 // var promise = new Promise(function (resolve){
-//   console.log("promise-1"); // 2
-//   resolve('promise');
-//   console.log("promise-2"); // 3
-// });
+//   console.log("promise-1") // 2
+//   resolve('promise')
+//   console.log("promise-2") // 3
+// })
 // var promise2 = new Promise(function (resolve){
-//   console.log("promise2-1"); // 4
-//   resolve('promise2');
-//   console.log("promise2-2"); // 5
-// });
+//   console.log("promise2-1") // 4
+//   resolve('promise2')
+//   console.log("promise2-2") // 5
+// })
 // promise2.then(function(value){
-//   console.log(10);   // 7
+//   console.log(10)   // 7
 //   console.log(value) // 8
-// });
+// })
 // promise.then(function(value){
-//   console.log(20);   // 9
+//   console.log(20)   // 9
 //   console.log(value) // 10
-// });
-// console.log("3");  // 6
+// })
+// console.log("3")  // 6
  
 // 1
 // promise-1
@@ -84,12 +84,12 @@ theRoom
 // new Promise 依序處理，然後 then() 是最後處理的
 
 /*--------------------------------------*/
-// Promise.resolve(5);
+// Promise.resolve(5)
 // //可以认为是以下代码的语法糖。
 
 // new Promise(function(resolve){
-//   resolve(5);
-// });
+//   resolve(5)
+// })
 
 //example
 // let okc = new Promise(function(resolve){
@@ -98,11 +98,11 @@ theRoom
 // })
 // okc
 // .then(a => {
-//   console.log(a);
+//   console.log(a)
 //   return Promise.resolve(3)
 // })
 // .then(a => {
-//   console.log(a);
+//   console.log(a)
 //   return Promise.resolve(4)
 // })
 // .then(a => {
